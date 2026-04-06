6fe7b44eb2f3524186f6b5e0f0693bc4e69a2d97
nayandeeppanika23-boop <nayandeeppanika23@gmail.com>
2026-04-06 22:09:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const canvas = document.getElementById("stars");
 const ctx = canvas.getContext("2d");
 const glow = document.querySelector(".glow");
+const exploreBtn = document.getElementById("exploreBtn");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let stars = [];
 
-for (let i = 0; i < 150; i++) {
+for (let i = 0; i < 180; i++) {
   stars.push({
     x: Math.random() * canvas.width,
-    y: Math.random() * canvas.height,
+    y: Math.random() * canvas.height * 2,
     size: Math.random() * 2,
-    speed: Math.random() * 0.5 + 0.2
+    speed: Math.random() * 0.7 + 0.2
   });
 }
 
@@ -24,7 +25,7 @@ function animate() {
   stars.forEach((star) => {
     star.y += star.speed;
 
-    if (star.y > canvas.height) {
+    if (star.y > document.body.scrollHeight) {
       star.y = 0;
       star.x = Math.random() * canvas.width;
     }
@@ -42,6 +43,26 @@ animate();
 window.addEventListener("mousemove", (e) => {
   glow.style.left = e.clientX + "px";
   glow.style.top = e.clientY + "px";
+});
+
+exploreBtn.addEventListener("click", () => {
+  document.getElementById("universe").scrollIntoView({
+    behavior: "smooth"
+  });
+});
+
+window.addEventListener("click", (e) => {
+  const ripple = document.createElement("div");
+  ripple.classList.add("ripple");
+
+  ripple.style.left = e.clientX - 150 + "px";
+  ripple.style.top = e.clientY - 150 + "px";
+
+  document.body.appendChild(ripple);
+
+  setTimeout(() => {
+    ripple.remove();
+  }, 800);
 });
 
 window.addEventListener("resize", () => {
